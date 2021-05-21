c9fa7a937db14b023e7950000c27c3811f09ef6b
exen11 <80745940+exen11@users.noreply.github.com>
2021-05-21 12:38:58 +0200

    eloXD

--- util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('node-complete', 'root', 'uef20220721',{
-    dialect: 'mysql',
-    host: 'localhost'
-});
-
-module.exports = sequelize;
